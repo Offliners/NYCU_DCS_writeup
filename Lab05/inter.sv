d94009951db61ef87e6c15367487688d8b18c8d4
Offliners <eric21904@gmail.com>
2023-11-12 00:20:24 +0800

    update Lab05 inter

--- Lab05/inter.sv
+++ Lab05/inter.sv
@@ -34,98 +34,126 @@ parameter S_idle = 2'b00,
           S_master2 = 2'b10,
           S_handshake = 2'b11;
 
-logic in_1, in_2, in_bef_1, in_bef_2;
-logic [6:0] data_1, data_2, data_bef_1, data_bef_2;
 logic [1:0] cur_state, next_state;
-logic [2:0] value_out_temp, addr_out_temp;
+logic in_valid_reg_1, in_valid_reg_2;
+logic [6:0] data_reg_1, data_reg_2;
 
 always_ff @ (posedge clk or negedge rst_n) begin
-  if(!rst_n) begin
+  if(!rst_n)
     cur_state <= S_idle;
-    in_bef_1 <= 0;
-    in_bef_2 <= 0;
-    data_bef_1 <= 0;
-    data_bef_2 <= 0;
-  end
-  else begin
+  else
     cur_state <= next_state;
-    in_bef_1 <= in_valid_1;
-    in_bef_2 <= in_valid_2;
-    data_bef_1 <= data_1;
-    data_bef_2 <= data_2;
-  end
+end
+
+always_ff @ (posedge clk or negedge rst_n) begin
+  if(!rst_n)
+    in_valid_reg_1 <= 0;
+  else if(cur_state == S_master1)
+    in_valid_reg_1 <= 0;
+  else if(in_valid_1)
+    in_valid_reg_1 <= in_valid_1;
+  else
+    in_valid_reg_1 <= in_valid_reg_1;
+end
+
+always_ff @ (posedge clk or negedge rst_n) begin
+  if(!rst_n)
+    in_valid_reg_2 <= 0;
+  else if(cur_state == S_master2)
+    in_valid_reg_2 <= 0;
+  else if(in_valid_2)
+    in_valid_reg_2 <= in_valid_2;
+  else
+    in_valid_reg_2 <= in_valid_reg_2;
+end
+
+always_ff @ (posedge clk or negedge rst_n) begin
+  if(!rst_n)
+    data_reg_1 <= 0;
+  else if(in_valid_1)
+    data_reg_1 <= data_in_1;
+  else
+    data_reg_1 <= data_reg_1;
+end
+
+always_ff @ (posedge clk or negedge rst_n) begin
+  if(!rst_n)
+    data_reg_2 <= 0;
+  else if(in_valid_2)
+    data_reg_2 <= data_in_2;
+  else
+    data_reg_2 <= data_reg_2;
+end
+
+always_ff @ (posedge clk or negedge rst_n) begin
+  if(!rst_n)
+    handshake_slave1 <= 0;
+  else if(next_state == S_handshake && (data_reg_1[6] == 0 || data_reg_2[6] == 0))
+    handshake_slave1 <= 1;
+  else
+    handshake_slave1 <= 0;
+end
+
+always_ff @ (posedge clk or negedge rst_n) begin
+  if(!rst_n)
+    handshake_slave2 <= 0;
+  else if(next_state == S_handshake && (data_reg_1[6] || data_reg_2[6]))
+    handshake_slave2 <= 1;
+  else
+    handshake_slave2 <= 0;
+end
+
+always_ff @ (posedge clk or negedge rst_n) begin
+  if(!rst_n)
+    valid_slave1 <= 0;
+  else if(next_state == S_master1 && data_reg_1[6] == 0)
+    valid_slave1 <= 1;
+  else if(next_state == S_master2 && data_reg_2[6] == 0)
+    valid_slave1 <= 1;
+  else
+    valid_slave1 <= 0;
+end
+
+always_ff @ (posedge clk or negedge rst_n) begin
+  if(!rst_n)
+    valid_slave2 <= 0;
+  else if(next_state == S_master1 && data_reg_1[6])
+    valid_slave2 <= 1;
+  else if(next_state == S_master2 && data_reg_2[6])
+    valid_slave2 <= 1;
+  else
+    valid_slave2 <= 0;
 end
 
 always_comb begin
-  in_1 = (in_valid_1) ? 1 : in_bef_1;
-  in_2 = (in_valid_2) ? 1 : in_bef_2;
-  data_1 = (in_valid_1) ? data_in_1 : data_bef_1;
-  data_2 = (in_valid_2) ? data_in_2 : data_bef_2;
-
   case(cur_state)
     S_idle:
-      if(in_1) begin
-        next_state = S_master1;
-        in_1 = 0;
-      end
-      else if(in_2) begin
-        next_state = S_master2;
-        in_2 = 0;
-      end
-      else
-        next_state = cur_state;
-    S_master1:
-      if(data_1[6] == 0) begin
-        if(valid_slave1 && ready_slave1) begin
-          next_state = S_handshake;
-          in_1 = 0;
-        end
-        else
-          next_state = cur_state;
-      end
-      else begin
-        if(valid_slave2 && ready_slave2) begin
-          next_state = S_handshake;
-          in_1 = 0;
-        end
-        else
-          next_state = cur_state;
-      end
-    S_handshake:
-      if(in_1)
-        next_state = S_master1;
-      else if(in_2)
-        next_state = S_master2;
-      else
-        next_state = cur_state;
+      next_state = (in_valid_reg_1) ? S_master1 : ((in_valid_reg_2) ? S_master2 : S_idle);
+    S_master1:  
+      next_state = (data_reg_1[6] == 0 && ready_slave1) ? S_handshake : ((data_reg_1[6] && ready_slave2) ? S_handshake : S_master1);
+    S_master2:  
+      next_state = (data_reg_2[6] == 0 && ready_slave1) ? S_handshake : ((data_reg_2[6] && ready_slave2) ? S_handshake : S_master2);  
     default:
-      next_state = cur_state;
+      next_state = S_idle;  
   endcase
 end
 
-always_comb begin
-  if(cur_state == S_master1) begin
-    value_out_temp = data_1[2:0];
-    addr_out_temp = data_1[5:3];
-  end
-  else if (cur_state == S_master2) begin
-    value_out_temp = data_2[2:0];
-    addr_out_temp = data_2[5:3];
-  end
-  else begin
-    value_out_temp = 0;
-    addr_out_temp = 0;
-  end
-end
-
 always_ff @ (posedge clk or negedge rst_n) begin
   if(!rst_n) begin
-    value_out <= 0;
     addr_out <= 0;
+    value_out <= 0;
+  end
+  else if (next_state == S_master1) begin
+    addr_out <= data_reg_1[5:3];
+    value_out <= data_reg_1[2:0];
+  end
+  else if(next_state == S_master2) begin
+    addr_out <= data_reg_2[5:3];
+    value_out <= data_reg_2[2:0];
   end
   else begin
-    value_out <= value_out_temp;
-    addr_out <= addr_out_temp;
+    addr_out <= 0;
+    value_out <= 0;
   end
 end
 
