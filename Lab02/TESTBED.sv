97dcc1dff5387327f1b2fb8d13c002eb1ef5ef27
Offliners <eric21904@gmail.com>
2024-06-12 01:08:47 +0800

    update lab02 pattern

--- Lab02/TESTBED.sv
+++ Lab02/TESTBED.sv
@@ -1,5 +1,6 @@
 `timescale 1us/100ns
 `include "PATTERN.sv"
+// `include "PATTERN_GEN.sv"
 `ifdef RTL
 `include "Sort.sv"
 `elsif GATE
@@ -15,12 +16,12 @@ logic [5:0] out_num;
 
 initial begin
   `ifdef RTL
-    $fsdbDumpfile("Sort.fsdb");
-	  $fsdbDumpvars(0,"+mda");
+    $dumpfile("Sort.vcd");
+	$dumpvars(1, I_Sort);
   `elsif GATE
-    $fsdbDumpfile("Sort_SYN.fsdb");
+	  $fsdbDumpfile("Sort_SYN.fsdb");
 	  $sdf_annotate("Sort_SYN.sdf",I_Sort);
-	  $fsdbDumpvars(0,"+mda");
+	  $fsdbDumpvars();
   `endif
 end
 
@@ -43,5 +44,16 @@ PATTERN I_PATTERN
 	.in_num4(in_num4),
 	.out_num(out_num)
 );
+
+// PATTERN_GEN I_PATTERN
+// (
+// 	.in_num0(in_num0),
+// 	.in_num1(in_num1),
+// 	.in_num2(in_num2),
+// 	.in_num3(in_num3),
+// 	.in_num4(in_num4),
+// 	.out_num(out_num)
+// );
+
 endmodule
 
--- README.md
+++ README.md
@@ -11,7 +11,7 @@ This repository contained my designs and patterns is used to practice the homewo
 
 ### Labs
 * Lab01 - Binary-Coded Decimal : [[spec](./Lab01/DCS_Lab01.pdf)] [[design](./Lab01/BCD.sv)] [[pattern](./Lab01/PATTERN_GEN.sv)]
-* Lab02 - Merge Sort : [[spec](./Lab02/DCS_Lab02.pdf)] [[design](./Lab02/Sort.sv)]
+* Lab02 - Merge Sort : [[spec](./Lab02/DCS_Lab02.pdf)] [[design](./Lab02/Sort.sv)] [[pattern](./Lab02/PATTERN_GEN.sv)]
 * Lab03 - Frequency Divider : [[spec](./Lab03/DCS_Lab03.pdf)] [[design](./Lab03/Counter.sv)]
 * Lab04 - Sequential Circuit : [[spec](./Lab04/DCS_Lab04.pdf)] [[design](./Lab04/Seq.sv)]
 * Lab05 - AHB Interconnect : [[spec](./Lab05/DCS_Lab05.pdf)] [[design](./Lab05/inter.sv)]
