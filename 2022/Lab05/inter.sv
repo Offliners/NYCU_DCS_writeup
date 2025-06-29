274138af4f166f50b1f3c40f1d130a8df538374d
Offliners <eric21904@gmail.com>
2025-06-29 22:36:01 +0800

    update 2022 Lab05

---
+++ 2022/Lab05/inter.sv
@@ -0,0 +1,30 @@
+module inter(
+  // Input signals
+  clk,
+  rst_n,
+  in_valid_1,
+  in_valid_2,
+  in_valid_3,
+  data_in_1,
+  data_in_2,
+  data_in_3,
+  ready_slave1,
+  ready_slave2,
+  // Output signals
+  valid_slave1,
+  valid_slave2,
+  addr_out,
+  value_out,
+  handshake_slave1,
+  handshake_slave2
+);
+
+//---------------------------------------------------------------------
+//   PORT DECLARATION
+//---------------------------------------------------------------------
+
+//---------------------------------------------------------------------
+//   YOUR DESIGN
+//---------------------------------------------------------------------
+
+endmodule
